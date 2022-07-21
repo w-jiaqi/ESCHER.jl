55ac35735d9291d2c97448760a8658127c2bf081
WhiffleFish <tylertronics@gmail.com>
2022-07-21 10:09:08 -0600

    smoke tests

---
+++ test/smoke.jl
@@ -0,0 +1,21 @@
+@testset "smoke" begin
+    game = Kuhn()
+    sol = ESCHERSolver(game)
+    train!(sol, 10)
+    vb = sol.value_buffer
+    @test length(vb.x) == length(vb.y) == length(vb)
+    @test length(vb) > 0
+
+    rb1 = first(sol.regret_buffer)
+    rb2 = last(sol.regret_buffer)
+    @test length(rb1.x) == length(rb1.y) == length(rb1)
+    @test length(rb1) > 0
+    @test length(rb2.x) == length(rb2.y) == length(rb2)
+    @test length(rb2) > 0
+
+    @test length(sol.strategy_buffer.x) == length(sol.strategy_buffer.y)
+    @test length(sol.strategy_buffer.x) > 0
+
+    h0 = initialhist(game)
+    @test ESCHER.value(sol, 1, vectorized_hist(game, h0)) isa Number
+end
